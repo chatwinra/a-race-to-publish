7dc351a6888d06143ae48650382db214c374bec2
Rich Chatwin <rac@sciencegamed.co.uk>
2013-09-08 11:14:35 +0100

    latest updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,86 @@
 module.exports = function(grunt) {
 
 	// Project configuration.
+			'use strict';
+
 	grunt.initConfig({
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		copy: {
+			build: {
+				src: 'src/CSVParser.js',
+				dest: 'build/CSVParser.js',
+				options: {
+					processContent: function ( content ) {
+						return content.replace( '<%= version %>', grunt.template.process( '<%= pkg.version %>' ) );
+					}
+				}
+			},
+			release: {
+				files: [{
+					cwd: 'build/',
+					src: [ '**' ],
+					expand: true,
+					dest: 'release/<%= pkg.version %>/'
+				}]
+			},
+			shortcut: {
+				files: [{
+					cwd: 'build/',
+					src: [ '**' ],
+					expand: true,
+					dest: ''
+				}]
+			},
+		
+		// Copy the files we need from the src folder to appfog/public
+		
+			generated: {
+				files: [{
+					expand: true,
+					cwd: 'project/styles',
+					src: ['**'],
+					dest: 'generated'
+				}]
+			},
+			root: {
+				files: [{
+					expand: true,
+					cwd: 'project/styles',
+					src: ['**'],
+					dest: 'appfog/public'
+				}]
+			}
+		},
+
+		concat: {
+			options: {
+				process: {
+					data: { version: '<%= pkg.version %>' }
+				}
+			},
+			legacy: {
+				src: [ 'src/CSVParser.js', 'src/json2.js' ],
+				dest: 'build/CSVParser-legacy.js'
+			}
+		},
+
+		uglify: {
+			main: {
+				src: 'build/CSVParser.js',
+				dest: 'build/CSVParser.min.js'
+			},
+			min: {
+				src: 'build/CSVParser-legacy.js',
+				dest: 'build/CSVParser-legacy.min.js'
+			}
+		},
+
+		qunit: {
+			main: 'test/index.html'
+		},
+	
 
 		// Main watch task. Kick this off by entering `grunt watch`. Now, any time you change the files below,
 		// the relevant tasks will execute
@@ -33,31 +112,29 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
+});
 		
-		  	fetch: {
-   				 options: {
-				      //file format depends on size: process: require( 'csv-to-json' )
-				    },
-				    table1: {
-				      url: 'https://docs.google.com/a/guardian.co.uk/spreadsheet/ccc?key=123456#gid=0',
-				      dest: 'project/data/table1.json'
-				    },
-				    table2: {
-				      url: 'https://docs.google.com/a/guardian.co.uk/spreadsheet/ccc?key=234567#gid=0',
-				      dest: 'project/data/table2.json'
-				    }
-				  }
+
 						
-	});
+
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
 
-	grunt.registerTask( 'build', [ 'default'] );
+	grunt.registerTask( 'build', [ 'default','copy:build', 'concat', 'uglify'] );
 
 		// Default task.
 	grunt.registerTask( 'default', [
 		
-		'sass'
+		'sass',
+		'copy',
+		'concat', 
+		'uglify'
+	
 		
 	]);
 
